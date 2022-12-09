b82b316027d973342c6bd0273c4d0712c9c0c8ad
wimvanhelden <wimvanhelden@hotmail.com>
2022-12-09 14:28:58 +0100

    update

--- database_functions.py
+++ database_functions.py
@@ -2,12 +2,43 @@
 
 #sqllite3 imported through databaseconnection
 import databaseconnection as db
+import sqlite3
 
 def show_table(tablename):
     try:
         # Write a query and execute it with cursor
         # Fetch and output resul
         query = 'select * from ' + tablename + ";"
+        count = db.cursor.execute(query)
+        result = db.cursor.fetchall()
+    
+        if len(result) == 0:
+            print('lege tabel!')
+        else:
+            print('output {}'.format(result))
+    except sqlite3.Error as error:
+        print('Error occured - ', error)
+
+def show_record(tablename, id):
+    try:
+        # Write a query and execute it with cursor
+        # Fetch and output resul
+        query = 'select * from ' + tablename + ' where pk_task_id =' + str(id) +";"
+        count = db.cursor.execute(query)
+        result = db.cursor.fetchall()
+    
+        if len(result) == 0:
+            print('lege tabel!')
+        else:
+            print('output {}'.format(result))
+    except sqlite3.Error as error:
+        print('Error occured - ', error)
+
+def show_field(tablename, id, fieldname):
+    try:
+        # Write a query and execute it with cursor
+        # Fetch and output resul
+        query = 'select ' + fieldname + ' from ' + tablename + ' where pk_task_id =' + str(id) +";"
         count = db.cursor.execute(query)
         result = db.cursor.fetchall()
     
--- main.py
+++ main.py
@@ -7,8 +7,9 @@ import database_functions as dbf  #database connection is made in through dbf
 def main():
     import databaseconnection as db
     #always rune make_connection first to establish connection with database
-    dbf.show_table('tasks') 
-
+    #dbf.show_table('tasks')
+    #dbf.show_record('tasks', 1)
+    print(type(dbf.show_field('tasks', 1, 'task_descr')))
     
 
 if __name__ == '__main__':
