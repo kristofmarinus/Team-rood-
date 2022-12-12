1ef951e05f2ffcd407dd88e8bc1acba12e30d903
samvanmalder <samvanmalder@hotmail.com>
2022-12-12 14:16:38 +0100

    no message

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ def main():
     #always rune make_connection first to establish connection with database
     #dbf.show_table('tasks')
     #dbf.show_record('tasks', 1)
-    print(dbf.show_field('tasks', 1, 'task_descr'))
+    #print(dbf.show_field('tasks', 4, 'task_descr'))
+    dbf.delete_record('tasks')
     
 
 if __name__ == '__main__':
