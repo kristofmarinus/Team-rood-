3ffdb22007688fdbc8bed52a44e2995dd911a65f
wimvanhelden <wimvanhelden@hotmail.com>
2023-01-05 02:54:04 +0100

    Improved getters and setters for Task

--- task.py
+++ task.py
@@ -1,5 +1,8 @@
 import database_functions as dbf
+import databaseconnection as db
 from baseclass import BaseClass
+import datetime
+import sqlite3
 
 
 class Task(BaseClass):
@@ -8,7 +11,7 @@ class Task(BaseClass):
     # also: how are we gonna handle None (Null) values?
     # it might be interesting to set a trigger in the database to keep track off when a task changes
     _tablename = 'tasks'
-<<<<<<< Updated upstream
+
     _id = None  
     _fk_project_id = None 
     _fk_user_id = None  
@@ -26,12 +29,15 @@ class Task(BaseClass):
     
     @id.setter
     def id(self, value):
-        if not isinstance(value, int):
-            raise TypeError('id moet een integer zijn')
-        elif value > 0:
-            self._id = value
-        else:
-            raise ValueError('id moet positief zijn')
+        # note: setting the ID to "none" is not allowed
+        try: 
+            value = int(value)
+            if value > 0:
+                self._id = value
+            else:
+                raise ValueError('id has to be positive')
+        except: 
+            raise TypeError('id has to be an integer, or a type that can be converted to an integer')
 
     @property
     def task_descr(self) -> str:
@@ -39,7 +45,14 @@ class Task(BaseClass):
 
     @task_descr.setter
     def task_descr(self, value):
-        self._task_descr = value
+        #note: setting task_descr to None/Null is not allowed
+        if value is None: 
+            raise TypeError("task_descr can not be None/Null")
+        else: 
+            try:
+                self._task_descr = str(value)
+            except: 
+                raise TypeError('task_descr has to be a string, or a type that can be converted to a string')
 
     @property
     def fk_project_id(self) -> int:
@@ -47,12 +60,17 @@ class Task(BaseClass):
 
     @fk_project_id.setter
     def fk_project_id(self, value: int):
-        if not isinstance(value, int):
-            raise TypeError('id moet een integer zijn')
-        elif value > 0:
-            self._fk_project_id = value
-        else:
-            raise ValueError('id moet positief zijn')
+        #note: setting fk_project_id to None is not allowed
+        try:
+            value = int(value)
+            if value > 0:
+                self._fk_project_id = value
+            else: 
+                raise ValueError('fk_project_id has to be positive')
+
+        except: 
+            raise TypeError('fk_project_id has to be an integer, or a type that can be converted to an integer')
+
 
     @property
     def fk_user_id(self) -> int:
@@ -60,12 +78,18 @@ class Task(BaseClass):
 
     @fk_user_id.setter
     def fk_user_id(self, value):
-        if not isinstance(value, int):
-            raise TypeError('id moet een integer zijn')
-        elif value > 0:
-            self._fk_user_id = value
+        if value is None:
+            self._fk_user_id = None
         else:
-            raise ValueError('id moet positief zijn')
+            try:
+                value = int(value)
+                if value > 0:
+                    self._fk_user_id = value
+                else: 
+                    raise ValueError('fk_user_id has to be positive')
+            except: 
+                raise TypeError('fk_user_id has to be Null/none, and integer, or a type that can be converted to an integer')
+        
 
     @property
     def task_date_added(self):
@@ -74,7 +98,7 @@ class Task(BaseClass):
         elif self._task_date_added is None:
             return None
         else:
-            raise TypeError('date paramater is in wrong type')
+            raise TypeError('date attribute is in wrong type')
 
     @task_date_added.setter
     def task_date_added(self, value):
@@ -114,13 +138,17 @@ class Task(BaseClass):
 
     @task_progress.setter
     def task_progress(self, value):
-        if isinstance(value, int):
-            if value >= 0 and value <= 100:
-                self._task_progress = value
-            else:
-                raise ValueError('progress has to be an integer [0-100]')
-        else:
-            raise TypeError('progress has to be an integer [0-100]')
+        if value is None: 
+            self._task_progress = None
+        else: 
+            try: 
+                value = int(value)
+                if value >= 0 and value <= 100:
+                    self._task_progress = value
+                else: 
+                    raise ValueError('task_progress has to be within [0,100]')
+            except: 
+                raise TypeError('task_progress has to be Null/None, an integer, or a type that can be converted to an integer')
 
     @property
     def task_started_on(self):
@@ -129,7 +157,7 @@ class Task(BaseClass):
         elif self._task_started_on is None:
             return None
         else:
-            raise TypeError('date paramater is in wrong type')
+            raise TypeError('date attribute is in wrong type')
 
     @task_started_on.setter
     def task_started_on(self, value):
@@ -176,10 +204,14 @@ class Task(BaseClass):
         # query to get the date from SQL
         # assing the variables to those values...
  
-    # function: to write TO SQL
-    # note: if we define the pointer in main, how does that inherit?
+
     @from_db.setter
     def from_db(self, id: int):
+        """sets the attributes to the values from the db. 
+        for example "new_task.from_db = 15" will set the attribute values of a task called new_task to the values from the database
+        Args:
+            id (int): the id of the record
+        """
         try:
             self.id = dbf.give_field('tasks', id, 'id')
             self.task_descr = dbf.give_field('tasks', id, 'task_descr')
@@ -192,27 +224,51 @@ class Task(BaseClass):
             self.task_finished_on = dbf.give_field('tasks', id, 'task_finished_on')
 
         except Exception as e:
-            print(f'error in module from_sql : {e}')
+            print(f'error in module from_db : {e}')
 
     def to_db(self):
-        # nog uitbreiden: als self.id Null is: nieuwe record wegschrijven. als self.id niet Null is: bestaande record updaten
-        # als we een nieuwe task aanmaken moeten we die GEEN id geven. Dat doet de database zelf bij het wegschrijven...
-        try:
-            query1 = f'insert into {self._tablename} '
-            query2= f'(task_descr, fk_user_id, fk_project_id, task_date_added, task_deadline, task_progress, task_started_on, task_finished_on) '
-            query3 = f'values '
-            query4 = f'("{self.task_descr}" , {self.fk_user_id} , {self.fk_project_id}, "{self.task_date_added}", "{self.task_deadline}", {self.task_progress},  "{self.task_started_on}", "{self.task_finished_on}");'.replace('"None"', 'Null').replace('None', 'Null')
-            query = query1 + query2 + query3 + query4
-            print(query)
-            count = db.cursor.execute(query)
-            db.sqliteConnection.commit()
-        except sqlite3.Error as error:
-            print('Error occured - ', error)
+        # we handle new records, and updating existing records
+        # if id is None: means it's a new task (not previously read from database)
+        # so to_db will create a new record (INSERT statement)
+        if self.id is None: 
+            try:
+                query1 = f'insert into {self._tablename} '
+                query2= f'(task_descr, fk_user_id, fk_project_id, task_date_added, task_deadline, task_progress, task_started_on, task_finished_on) '
+                query3 = f'values '
+                query4 = f'("{self.task_descr}" , {self.fk_user_id} , {self.fk_project_id}, "{self.task_date_added}", "{self.task_deadline}", {self.task_progress},  "{self.task_started_on}", "{self.task_finished_on}");'.replace('"None"', 'Null').replace('None', 'Null')
+                query = query1 + query2 + query3 + query4
+                count = db.cursor.execute(query)
+                db.sqliteConnection.commit()
+            except sqlite3.Error as error:
+                print('Error occured - ', error)
+        #if ID is not none: means the record most likely CAME from the database or existing record has to be changed. so an UPDATE statement is used
+        if not self.id is None: 
+            try:
+                #note: the id is NOT updated
+                query1 = f'update {self._tablename} '
+                query2 = f'set task_descr = "{self.task_descr}", fk_user_id = {self.fk_user_id}, fk_project_id = {self.fk_project_id}, task_date_added = "{self.task_date_added}", '
+                query3 = f'task_deadline = "{self.task_deadline}", task_progress = "{self.task_progress}", task_started_on = "{self.task_started_on}", task_finished_on = "{self.task_finished_on}" '
+                query4 = f'where id = {self.id}'
+                query_together = query1 + query2 + query3 + query4
+                #cleaning up the query for SQL synstax:
+                query = query_together.replace('"None"', 'Null').replace('None', 'Null')
+                print(query)
+                count = db.cursor.execute(query)
+                db.sqliteConnection.commit()
+            except sqlite3.Error as error:
+                print('Error occured - ', error)
+
 
     def __str__(self):
         return self.__dict__()
 
 # funtion to CHANGE a task (like assign user)
 # should be two functions: get input + change task
+def main():
+    pass
+   
 
-new_task = Task()
+
+
+if __name__ == '__main__':
+    main()
